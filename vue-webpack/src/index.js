2a414af92c3e23e37f5f42bbc21379481d2bc2dc
lq <1211@qq.com>
2019-07-24 21:08:24 +0800

    123

---
+++ vue-webpack/src/index.js
@@ -0,0 +1,32 @@
+// 以模块的方式引入vue
+const Vue = require('vue');
+const vm = new Vue({
+  
+    el: '#demo',
+
+})
+    // M
+    // data: {
+       
+    // },
+    // 模板
+    // template: `
+    //     <div>
+        
+    //     </div>
+    // `,
+    // methods: {
+    //     test(num) {
+    //         // V->M->V
+    //         // 把M层里面name改为jing，由于M变，V会跟着变
+    //         vm.name = 'jing'
+    //         // console.log(num)
+    //     },
+    //     test2() {
+    //         console.log(2)
+    //     },
+    //     // 使用回车键触发
+    //     onEnter() {
+    //         console.log('input')
+    //     }
+    // }
